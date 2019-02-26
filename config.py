4294bcffa7c3f2b321062c8c442c1f00d1d4d036
ugiriwabo <ugiraconsole@gmail.com>
2019-02-26 14:55:48 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+class Config:
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:12345@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # SUBJECT_PREFIX = 'Watchlist'
+    # SENDER_EMAIL = 'ugiraconsoleee@gmail.com'
+    
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
